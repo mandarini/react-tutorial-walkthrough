2c5c4d32d4d85fd126ba150966eb01c17e84df4d
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-02-16 18:04:55 +0200

    commiting to test affected

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Banner } from '@myorg/common-ui';
+
+export function App() {
+  return (
+    <>
+      <Banner text="Welcome to our admin app." />
+      <div />
+    </>
+  );
+}
+
+export default App;
--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,12 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { Banner } from '@myorg/common-ui';
+import { exampleProducts } from '@myorg/products';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function App() {
   return (
     <>
-      <NxWelcome title="store" />
-      <div />
+      <Banner text="Welcome to the store!" />
+      <ul>
+        {exampleProducts.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> Price: {product.price}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
